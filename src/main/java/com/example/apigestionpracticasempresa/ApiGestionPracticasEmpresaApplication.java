a30e11b6dbcad14d339377ef48e0a319e031fc3e
jorgeoj <jorge2002oj@gmail.com>
2024-02-19 16:01:26 +0100

    Initial commit

---
+++ src/main/java/com/example/apigestionpracticasempresa/ApiGestionPracticasEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigestionpracticasempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionPracticasEmpresaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGestionPracticasEmpresaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apigestionpracticasempresa/ApiGestionPracticasEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigestionpracticasempresa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionPracticasEmpresaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
